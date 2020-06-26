c2e2580d8a2c79ff08fe14479af6bc0da9241c8a
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-06-26 03:21:23 +0000

    [Update]Version 0.1 of the hbnb CLI, missing some functions like all and update

--- console.py
+++ console.py
@@ -5,6 +5,10 @@ Contains the hbnb command interpreter
 
 
 import cmd
+import models
+import sys
+from models.base_model import BaseModel
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +26,73 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, argum):
         """This makes that you exit from the command interpreter"""
         return True
+
+    def do_create(self, argum):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and
+prints the id."""
+        if len(argum) == 0:
+            print("** class name missing **")
+            return
+        argum_list = argum.split()
+        try:
+            instance = eval(argum_list[0])()
+            print(instance.id)
+            instance.save()
+        except:
+            print("** class doesn't exist **")
+            return
+
+    def do_show(self, argum):
+        """Prints the string representation of an instance based on the class
+name and id."""
+        if len(argum) == 0:
+            print("** class name missing **")
+            return
+        argum_list = argum.split()
+        try:
+            instance = eval(argum_list[0])
+        except:
+            print("** class doesn't exist **")
+            return
+        if len(argum_list) == 1:
+            print("** instance id missing **")
+            return
+        elif len(argum_list) > 1:
+            key = argum_list[0] + "." + argum_list[1]
+            if key in models.storage.all():
+                print(models.storage.all()[key])
+            else:
+                print("** no instance found **")
+                return
+
+    def do_destroy(self, argum):
+        """Deletes an instance based on the class name and id"""
+        if len(argum) == 0:
+            print("** class name missing **")
+            return
+        argum_list = argum.split()
+        try:
+            instance = eval(argum_list[0])
+        except:
+            print("** class doesn't exist **")
+            return
+        if len(argum_list) == 1:
+            print("** instance id missing **")
+            return
+        elif len(argum_list) > 1:
+            key = argum_list[0] + "." + argum_list[1]
+            if key in models.storage.all():
+                models.storage.all().pop(key)
+                models.storage.save()
+            else:
+                print("** no instance found **")
+                return
+
+    def do_all(self, argum):
+        """Prints all string representation of all instances based or not on the
+class name."""
+        
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
