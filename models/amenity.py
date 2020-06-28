ddd9d78af24ef2fe6b0dcc5f13151b2e5eaf2976
Fredy <fredhiixd@gmail.com>
2020-06-28 16:42:57 -0500

    Finish feature_mandatory_tasks

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Amenity Model | inherites from BaseModel """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity class """
+    name = ''
