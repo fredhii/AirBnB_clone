9fb5ba2c2f5d1cd8f70daa632c709e56590a5eec
Fredy <fredhiixd@gmail.com>
2020-06-30 19:35:08 -0500

    Return test cases

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" Amenity Model Unit Test """
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenityFunctionality(unittest.TestCase):
+    """ Test amenity """
+    def setUp(self):
+        """ Creates an instance before each test """
+        self.test = Amenity()
+
+    def tearDown(self):
+        """ Deletes instance after a test """
+        del self.test
+
+    def test_id_type(self):
+        """ Check ID type """
+        self.assertEqual(type(self.test.id), str)
+
+    def test_unique_id(self):
+        """ Check if generates a UUID for every instance """
+        test2 = Amenity()
+        self.assertNotEqual(self.test.id, test2.id)
+
+    def test_created_at_type(self):
+        """ Check created_at type """
+        self.assertEqual(type(self.test.created_at), datetime)
+
+    def test_updated_at_type(self):
+        """ Check updated_at type """
+        self.assertEqual(type(self.test.updated_at), datetime)
+
+    def test_amenity_type(self):
+        """ Check amenity type """
+        self.assertEqual(type(Amenity.name), str)
+
+    def test_save(self):
+        """ Test save function """
+        check = self.test.updated_at
+        self.test.save()
+        self.assertNotEqual(check, self.test.updated_at)
+
+    def test_str(self):
+        """ Check __str__ return """
+        self.assertEqual(str(self.c1), "[Amenity] ({}) {}".
+                         format(self.c1.id, self.c1.__dict__))
+
+    def test_dictionary(self):
+        """ Check to_dict function """
+        dictionary = self.test.to_dict()
+        self.assertEqual(type(dictionary), dict)
+        self.assertTrue(hasattr(dictionary, '__class__'))
+        self.assertEqual(type(dictionary['created_at']), str)
+        self.assertEqual(type(dictionary['updated_at']), str)
+
+    def test_kwargs(self):
+        """ Validate kawrgs arguments """
+        dictionary = self.test.to_dict()
+        test2 = Amenity(**dictionary)
+        self.assertEqual(self.test.id, test2.id)
+        self.assertEqual(self.test.created_at, test2.created_at)
+        self.assertEqual(self.test.updated_at, test2.updated_at)
+        self.assertNotEqual(self.test, test2)
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" City Model Unit Test """
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.city import City
+
+
+class TestCityModel(unittest.TestCase):
+    """ Test City """
+    def setUp(self):
+        """ Creates an instance before each test """
+        self.test = City()
+
+    def tearDown(self):
+        """ Deletes instance after a test """
+        del self.test
+
+    def test_id_type(self):
+        """ Check ID type """
+        self.assertEqual(type(self.test.id), str)
+
+    def test_unique_id(self):
+        """ Check if generates a UUID for every instance """
+        test2 = City()
+        self.assertNotEqual(self.test.id, test2.id)
+
+    def test_created_at_type(self):
+        """ Check created_at type """
+        self.assertEqual(type(self.test.created_at), datetime)
+
+    def test_updated_at_type(self):
+        """ Check updated_at type """
+        self.assertEqual(type(self.test.updated_at), datetime)
+
+    def test_city_type(self):
+        """ Check city type """
+        self.assertEqual(type(City.name), str)
+
+    def test_state_id_type(self):
+        """ Check state_id type """
+        self.assertEqual(type(City.state_id), str)
+
+    def test_save(self):
+        """ Test save function """
+        check = self.test.updated_at
+        self.test.save()
+        self.assertNotEqual(check, self.test.updated_at)
+
+    def test_str(self):
+        """ Check __str__ return """
+        self.assertEqual(str(self.test), "[City] ({}) {}".
+                         format(self.test.id, self.test.__dict__))
+
+    def test_dictionary(self):
+        """ Check to_dict function """
+        dictionary = self.test.to_dict()
+        self.assertEqual(type(dictionary), dict)
+        self.assertTrue(hasattr(dictionary, '__class__'))
+        self.assertEqual(type(dictionary['created_at']), str)
+        self.assertEqual(type(dictionary['updated_at']), str)
+
+    def test_kwargs(self):
+        """ Validate kawrgs arguments """
+        dictionary = self.test.to_dict()
+        test2 = City(**dictionary)
+        self.assertEqual(self.test.id, test2.id)
+        self.assertEqual(self.test.created_at, test2.created_at)
+        self.assertEqual(self.test.updated_at, test2.updated_at)
+        self.assertNotEqual(self.test, test2)
